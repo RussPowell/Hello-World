ad6cf7537bc531e56b7ea347d7e19edb78b06dcb
RussPowell <rpowell008@student.butte.edu>
2016-04-24 18:35:01 +0000

    complete

---
+++ current/Topic_4/lab3/BST_node.cpp
@@ -0,0 +1,77 @@
+/*
+ * Name        : BST_node.cpp
+ * Author      : Russell Powell
+ * Links       : https://github.com/RussPowell/Hello-World.git
+ *               https://hello-world-russpowell.c9users.io
+ *
+ * Description : Creating a tree
+ */
+
+#include "BST_node.h"
+
+
+BSTNode::BSTNode()
+{
+     left_child_ = NULL;
+     right_child_ = NULL;
+     contents_ = 0;
+}
+
+BSTNode::BSTNode(int newContents)
+{
+    left_child_ = NULL;
+    right_child_ = NULL;
+    contents_ = newContents;
+}
+
+BSTNode::~BSTNode()
+{
+    left_child_ = NULL;
+    right_child_ = NULL;
+}
+
+void BSTNode::set_contents(int newContents)
+{
+    contents_ = newContents;
+}
+
+int BSTNode::contents() const
+{
+    return contents_;
+}
+
+int& BSTNode::contents()
+{
+    return contents_;
+}
+
+
+void BSTNode::set_left_child(BSTNode* newLeft)
+{
+    left_child_ = newLeft;
+}
+
+void BSTNode::set_right_child(BSTNode* newRight)
+{
+    right_child_ = newRight;
+}
+
+BSTNode* BSTNode::left_child() const
+{
+    return left_child_;
+}
+
+BSTNode*& BSTNode::left_child()
+{
+    return left_child_;
+}
+
+BSTNode* BSTNode::right_child() const
+{
+    return right_child_;
+}
+
+BSTNode*& BSTNode::right_child()
+{
+    return right_child_;
+}
